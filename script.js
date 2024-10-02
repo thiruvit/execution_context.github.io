edc2b95598759add3a448ab2668370dd7dc17ade
thiru4098 <thirunavukarasuonline@gmail.com>
2024-10-02 06:17:18 +0530

    Execution Context in Js

--- script.js
+++ script.js
@@ -1,78 +1,121 @@
+var name = "Thiru";
+let role = "Coffee Shop Manager";
 
-var firstMessage="Hi";
-var lastMessage=" everyone";
+function CoffeeShop(){
+    let shopName = "Cafe Wild Beans";
+    const menu = ["Espresso", "Latte", "Cappuccino"];
 
-function getMessage() {
-    console.log("Message: " +firstMessage);
+    const employeeProfile = name + " - " + role;
+
+    console.log(employeeProfile);
+
+    function dailySales(){
+        let cupsSold = "50";
+        let averagePrice = "4";
+
+        console.log(cupsSold * averagePrice);
+    }
+    dailySales();
 }
-getMessage();
-var message=firstMessage+lastMessage;
-console.log(message);
+CoffeeShop();
 
 
-//Global Execution Context
+/*
+GLOBAL EXECUTION CONTEXT
 
-//Creation Phase
+CREATION PHASE
 
-GEC:{
-    LEC:{
-        ER:{
-               firstMessage:undefined;
-               lastMessage:undefined;
-        }
-        outer:null;
-        this:window Object;``
-    }
-    VE:{
-        ER:{
-                message:undefined;
-        }
-    }
-}
-//Execution Phase
-
-GEC:{
-    LEC:{
-        ER:{
-               firstMessage:"Hi";
-               lastMessage:"everyone";
-        }
-        outer:null;
-        this:window Object;
-    }
-    VE:{
-        ER:{
-                 message:Hi everyone;
-        }
+GlobalExecutionContext={
+   LexicalEnvironment:{
+     EnvironmentRecord:{
+        name : undefined,(Declarative)
+        role :declared but not initialized.(Declarative)
+        function:CoffeeShop();(Object)
+     },
+     outer: null
+     ThisBinding: window object
     }
 }
 
-//Function Execution Context
+EXECUTION PHASE
 
-//Creation Phase
-
-GEC:{
-    LEC:{
-        ER:{
-
-                 firstMessage:undefined;
-        }
-        outer:GEC;
-        this:window Object;
-    }
-}
-
-//Execution Phase
-
-GEC:{
-    LEC:{
-        ER:{
-                  firstMessage:Hi;
-        }
-        outer:GEC;
-        this:window Object;
+GlobalExecutionContext={
+    LexicalEnvironment:{
+      EnvironmentRecord:{
+         name : "Thiru"
+         role : "Coffee Shop Manager"
+         function:CoffeeShop();
+      },
+      outer: null
+      ThisBinding: window object
     }
 }
 
 
 
+FUNCTION EXECUTION CONTEXT-CoffeeShop()
+
+CREATION PHASE
+
+FunctionExecutionContext={
+   LexicalEnvironment:{
+     EnvironmentRecord:{
+        shopName : declared but not initialized.(declarative)
+        menu : declared but not initialized.(object)
+        employeeProfile : declared but not initialized.(object)
+        function:dailySales() (object)
+     },
+     outer: GlobalExecutionContext
+     ThisBinding: CoffeeShop()
+    }
+}
+
+
+EXECUTION PHASE
+
+FunctionExecutionContext={
+   LexicalEnvironment:{
+     EnvironmentRecord:{
+        shopName : "Cafe Wild Beans" (declarative)
+        menu :["Espresso", "Latte", "Cappuccino"] (object)
+        employeeProfile : "Thiru - Coffee Shop Manager" (object)
+        console.log(employeeProfile); // output "Thiru - Coffee Shop Manager"
+        function:dailySales() (object)
+     },
+     outer: GlobalExecutionContext
+     ThisBinding: CoffeeShop()
+    }
+}
+
+
+
+FUNCTION EXECUTION CONTEXT-dailySales()
+
+CREATION PHASE
+
+FunctionExecutionContext={
+   LexicalEnvironment:{
+     EnvironmentRecord:{
+        cupsSold : declared but not initialized. (declarative)
+        averagePrice : declared but not initialized. (declarative)
+     },
+     outer: CoffeeShop() Execution Context
+     ThisBinding: dailySales()
+    }
+}
+
+EXECUTION PHASE
+
+FunctionExecutionContext={
+   LexicalEnvironment:{
+     EnvironmentRecord:{
+        cupsSold : "50"  (declarative)
+        averagePrice : "4"  (declarative)
+        console.log(cupsSold * averagePrice); // outputs 200
+     },
+     outer: CoffeeShop() Execution Context
+     ThisBinding: dailySales()
+    }
+}
+
+*/
